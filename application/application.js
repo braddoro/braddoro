a3f120d85eb81b3c26d070f54441840c26a339b0
braddoro <braddoro@39d16025-4845-0410-9430-b141b9a394e6>
2008-02-29 15:39:00 +0000

    moving files around

--- application/application.js
+++
@@ -1,7 +0,0 @@
-function js_requestApplication(Task, container, itemID) {
-	var sPostString = "";
-	sPostString += "Task=" + Task + "&";
-	sPostString += "itemID=" + itemID + "&";
-	sPostString = sPostString.replace("%","%25");
-	document.getElementById(container).innerHTML = http_post_request("/braddoro/application/application_ajax.cfm",sPostString);
-}
