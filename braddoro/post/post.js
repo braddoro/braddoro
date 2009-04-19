b73d081f7c1ba6025751981d41301e9e6795a401
braddoro <braddoro@39d16025-4845-0410-9430-b141b9a394e6>
2009-04-19 21:38:52 +0000

    changed how I handle special characters.

--- braddoro/post/post.js
+++ braddoro/post/post.js
@@ -5,20 +5,19 @@ function js_requestPost(Task, container, itemID) {
 	if (Task == "addPost" || Task == "updatePost") {
 		sPostString += "TopicID=" + document.getElementById("topicID").value + "&";
 		sPostString += "Subject=" + encodeURIComponent(document.getElementById("subject").value) + "&";
-		sPostString += "Post=" + encodeURIComponent(document.getElementById("post").value.replace(/\r\n/g,"<br>")) + "&";
+		sPostString += "Post=" + encodeURIComponent(document.getElementById("post").value) + "&";
 	}
 	if (Task == "getSearch") {
-		sPostString += "TopicID=" + encodeURIComponent(document.getElementById("topicFilter").value) + "&";
+		sPostString += "TopicID=" + document.getElementById("topicFilter").value + "&";
 		sPostString += "Filter=" + encodeURIComponent(document.getElementById("filter").value) + "&";
 		sPostString += "postID=" + document.getElementById("postID").value + "&";
 	}
 	if (Task == "saveReply") {
-		sPostString += "replyText=" + encodeURIComponent(document.getElementById("replytext_"+itemID).value.replace(/\r\n/g,"<br>")) + "&";
+		sPostString += "replyText=" + encodeURIComponent(document.getElementById("replytext_"+itemID).value) + "&";
 	}
 	if (Task == "updateReply") {
-		sPostString += "replyText=" + encodeURIComponent(document.getElementById("replytext_"+itemID).value.replace(/\r\n/g,"<br>")) + "&";
+		sPostString += "replyText=" + encodeURIComponent(document.getElementById("replytext_"+itemID).value) + "&";
 		sPostString += "replyID=" + document.getElementById("postReplyID_"+itemID).value + "&";
 	}
-	sPostString = sPostString.replace("%","%25");
 	document.getElementById(container).innerHTML = http_post_request("/braddoro/post/post_ajax.cfm",sPostString);
 }
